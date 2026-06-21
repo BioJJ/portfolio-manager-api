1e83c89dca14bb890fed8422e040c5004e5e0de9
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2026-06-21 19:00:15 -0400

    feat: ⚙️ Set up API security, OpenAPI, and exception handling
    Add foundational configurations for the API, including:
    - Spring Security with basic authentication, in-memory user, and public access for Swagger UI.
    - OpenAPI (Swagger) documentation setup with API info and basic auth security scheme.
    - Global exception handling for `NotFoundException`, `BusinessException`, and validation errors, providing consistent error responses.

---
+++ src/main/java/biojj/portfoliomanagerapi/PortfolioManagerApi.java
@@ -0,0 +1,11 @@
+package biojj.portfoliomanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioManagerApi {
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioManagerApi.class, args);
+    }
+}
